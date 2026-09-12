7214cea10af63574c6bba433e81d6bc04f72fde7
theabmmohi <theabmmohi@gmail.com>
2026-09-12 12:51:41 +0600

    Secured :)

--- web/bootstrap/app.php
+++ web/bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
+        // For Render Deployment...
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
