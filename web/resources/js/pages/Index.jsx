7e5011fdf28b6e7ff5385c9c358e654d7de52b4c
theabmmohi <theabmmohi@gmail.com>
2026-09-15 22:59:37 +0600

    Commit

--- web/resources/js/pages/Index.jsx
+++ web/resources/js/pages/Index.jsx
@@ -1,5 +1,4 @@
-import { Button } from "@/components/ui/button"
-import { usePage, router } from "@inertiajs/react"
+import { usePage } from "@inertiajs/react"
 export default function Index () {
   const { props } = usePage()
   return <>
