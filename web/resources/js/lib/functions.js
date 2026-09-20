11a370300b2da669a2c13d70cd56f7f344b931e0
theabmmohi <theabmmohi@gmail.com>
2026-09-20 22:29:12 +0600

    bowser

--- web/resources/js/lib/functions.js
+++ web/resources/js/lib/functions.js
@@ -1,3 +1,5 @@
+import bowser from "bowser"
+
 export const down = () => {
   if (document.activeElement instanceof HTMLElement) {
     document.activeElement.blur()
@@ -29,23 +31,10 @@ export const ago = (stamp) => {
 }
 
 export const guessDevice = () => {
-  if (typeof navigator === "undefined") return ""
-  const ua = navigator.userAgent
-  const os =
-    /Windows/.test(ua) ? "Windows" :
-    /iPhone|iPad|iPod/.test(ua) ? "iOS" :
-    /Macintosh/.test(ua) ? "Mac" :
-    /Android/.test(ua) ? "Android" :
-    /Linux/.test(ua) ? "Linux" :
-    null
-  const br =
-    /Edg\//.test(ua) ? "Edge" :
-    /OPR\//.test(ua) ? "Opera" :
-    /SamsungBrowser/.test(ua) ? "Samsung Internet" :
-    /Chrome\//.test(ua) ? "Chrome" :
-    /Firefox\//.test(ua) ? "Firefox" :
-    /Safari\//.test(ua) ? "Safari" :
-    null
+  if (typeof window === "undefined" || typeof navigator === "undefined") return
+  const parser = bowser.getParser(window.navigator.userAgent)
+  const br = parser.getBrowserName()
+  const os = parser.getOSName()
   if (br && os) return `${br} on ${os}`
   return br || os || ""
 }
